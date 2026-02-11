e7baa556b2e8e17c0f186219a87c5c142dfebbab
andymdavid <andy@otherstuff.studio>
2026-02-11 18:54:29 +0800

    Add generated Level-Up styles and font vars

--- src/routes/LevelUp.tsx
+++ src/routes/LevelUp.tsx
@@ -1,9 +1,10 @@
 import { SiteShell } from '@/levelup/components/sections/site-shell';
-import '@/levelup/levelup.css';
+import '@/levelup/levelup.fonts.css';
+import '@/levelup/levelup.generated.css';
 
 export default function LevelUp() {
   return (
-    <div className="levelup-theme min-h-screen font-sans">
+    <div className="levelup-theme levelup-theme-vars min-h-screen font-sans">
       <SiteShell />
     </div>
   );
